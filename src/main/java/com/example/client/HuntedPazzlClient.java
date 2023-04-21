7af5d2af66dd2ece6814258f41a9d64fecce063c
Abdul-Razaq Adam <raino.red@gmail.com>
2023-04-21 20:25:30 +0300

    Adjust Client interface (squashed)
    Adjust Client interface.

--- src/main/java/com/example/client/HuntedPazzlClient.java
+++ src/main/java/com/example/client/HuntedPazzlClient.java
@@ -6,6 +6,7 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -27,11 +28,8 @@ public class HuntedPazzlClient extends Application {
                 String playerName = nameField.getText();
                 // TODO: Implement game screen and logic (Need client interface)
                 createGame(playerName); // Aplication works (Once)
-                // Close window after.
                 Stage stage = (Stage) playButton.getScene().getWindow();  // get a handle to the stage
-                stage.hide();  // do what you have to do
-
-                
+                stage.hide(); // Close window after.
 
             } catch (IOException e) {
                 System.err.println("Not Connected to Server!");;
@@ -41,31 +39,29 @@ public class HuntedPazzlClient extends Application {
         VBox root = new VBox();
         root.getChildren().addAll(label, nameField, playButton);
 
-        Scene scene = new Scene(root, 300, 250);
+        Scene scene = new Scene(root, 250, 200);
+        root.setAlignment(Pos.CENTER);
         primaryStage.setTitle("Hunted Pazzl");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
     // Need rework
     private void createGame(String playerName) throws IOException {
+        // Client Recognise the server
         URL url = new URL(SERVER_URL );
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("POST");
         connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
         connection.setDoOutput(true);
-        // writing to server (Client doesn't write to server)
-        // This code tells the server client's name.
+        // Tells the user client's name.
         String jsonInputString = "{\"playerName\": \"" + playerName + "\"}";
         System.out.println(jsonInputString);
       
         // Read from server (Client should take information from server)
-        // It should request the game from server.
         try (InputStream inputStream = connection.getInputStream()) {
             String response = new String(inputStream.readAllBytes());
             System.out.println(response);
-        
         }
-        
     }
 
     public static void main(String[] args) {
