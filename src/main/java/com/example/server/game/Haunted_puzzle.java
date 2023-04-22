ffb04fd3322646e8ee4ecd0b8f48c726582f7d80
Abdul-Razaq Adam <raino.red@gmail.com>
2023-04-22 03:29:41 +0300

    Solve/Blank buttons are functional.

--- src/main/java/com/example/server/game/Haunted_puzzle.java
+++ src/main/java/com/example/server/game/Haunted_puzzle.java
@@ -18,6 +18,7 @@ public class Haunted_puzzle  implements ActionListener{
     private static JPanel panel,btnPanel;
     private static BufferedImage myUnsolved, mySolved;
     private static JLabel picLabel;
+    private static JLayeredPane layeredPane,gameLayeredPane;
 
     /**
      * @param playerNameString get Name from Client.
@@ -26,11 +27,12 @@ public class Haunted_puzzle  implements ActionListener{
     private static void Game(String playerNameString) throws IOException{
         frame = createFrame("Haunted Puzzle Game: " + playerNameString); // Name of puzzle and player.
         myUnsolved = ImageIO.read(new File("C:/Users/me/OneDrive - University of Prince Mugrin/Construction/Project_Phase_One/demo/src/main/resources/com/example/server/game/Unsolved.jpg"));
-        mySolved = ImageIO.read(new File("C:/Users/me/OneDrive - University of Prince Mugrin/Construction/Project_Phase_One/demo/src/main/resources/com/example/server/game/Unsolved.jpg"));
+        mySolved = ImageIO.read(new File("C:/Users/me/OneDrive - University of Prince Mugrin/Construction/Project_Phase_One/demo/src/main/resources/com/example/server/game/Solved.jpg"));
         picLabel = new JLabel(new ImageIcon(myUnsolved)); // Set unsolved state.
 
         
         btnPanel = createBtnPanel();
+        gameLayeredPane = createGameLayeredPane();
         panel = createGamePanel();
 
         btn = createButton("Solve");
@@ -38,30 +40,39 @@ public class Haunted_puzzle  implements ActionListener{
         btnPanel.add(btn);
         btn.addActionListener(new Haunted_puzzle(){
             public void actionPerformed(ActionEvent arg0) {
-                
-                if (arg0.getSource() == btn){
-                    JOptionPane.showMessageDialog(frame, "this is ");
-                    // throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
-                }
+                picLabel = new JLabel(new ImageIcon(mySolved));
+                panel = createGamePanel();
+
+
+                gameLayeredPane.removeAll();
+                panel.add(picLabel);
+                gameLayeredPane.add(panel);
+                gameLayeredPane.repaint();
+                gameLayeredPane.revalidate();
             }
         });
-        
         btn2 = createButton("Blank");
         btn2.setLocation(303, 10);
         btnPanel.add(btn2);
         btn2.addActionListener(new Haunted_puzzle(){
             public void actionPerformed(ActionEvent arg0) {
                 
-                if (arg0.getSource() == btn2){
-                    JOptionPane.showMessageDialog(frame, "this is ... ");
-                    // throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
-                }
+                picLabel = new JLabel(new ImageIcon(myUnsolved));
+                panel = createGamePanel();
+
+                gameLayeredPane.removeAll();
+                panel.add(picLabel);
+                gameLayeredPane.add(panel);
+                gameLayeredPane.repaint();
+                gameLayeredPane.revalidate();;
+
             }
         });
         frame.add(btnPanel);        
         
         panel.add(picLabel);
-        frame.add(panel);
+        gameLayeredPane.add(panel);
+        frame.add(gameLayeredPane);
         frame.setVisible(true); // Visible.
     }
     /**
@@ -72,8 +83,9 @@ public class Haunted_puzzle  implements ActionListener{
         JFrame.setDefaultLookAndFeelDecorated(true);
         frame = new JFrame(gameNameString);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setBounds(0, 0, 626, 790);
-        frame.setResizable(false);
+        frame.setBounds(0, 0, 626, 790);//790
+        frame.setLocationRelativeTo(null);
+        //frame.setResizable(false);
         frame.setLayout(null);
         return frame;
     }
@@ -84,10 +96,16 @@ public class Haunted_puzzle  implements ActionListener{
      */
     private static JButton createButton(String texString) {    
         button = new JButton(texString, null);
-        button.setHorizontalAlignment(JButton.CENTER);
         button.setSize(90, 20);
 
         return button;
+    }
+    private static JLayeredPane createGameLayeredPane() {
+        layeredPane = new JLayeredPane();
+        layeredPane.setBounds(0, 40, 616, 750);
+        layeredPane.setBackground(Color.RED);
+        layeredPane.setLayout(null);
+        return layeredPane;
     }
     /**
      * @return
@@ -95,8 +113,8 @@ public class Haunted_puzzle  implements ActionListener{
      */
     private static JPanel createGamePanel() {
         panel = new JPanel();
-        panel.setSize(616, 750);
-        panel.setLocation(0, 40); //15 40
+        panel.setSize(616, 720);
+        // panel.setLocation(0, 40); //15 40
         panel.setBackground(Color.BLACK);
         return panel; 
     }
