af110380f362b8e671491cf0bc898872178fe1bf
Abdul-Razaq Adam <raino.red@gmail.com>
2023-04-25 01:09:04 +0300

    More Documentation.
    Few changes added from team members.

--- src/main/java/com/example/server/HuntedPazzlServer.java
+++ src/main/java/com/example/server/HuntedPazzlServer.java
@@ -19,40 +19,48 @@ public class HuntedPazzlServer {
     }
 
     public void run() throws Exception {
+        // create new event loop group for handling incoming connections
         EventLoopGroup bossGroup = new NioEventLoopGroup();
+        // create new event loop group for handling actual I/O operations
         EventLoopGroup workerGroup = new NioEventLoopGroup();
         try {
+            // create server bootstrap instance and configure it
             ServerBootstrap serverBootstrap = new ServerBootstrap()
                     .group(bossGroup, workerGroup)
                     .channel(NioServerSocketChannel.class)
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         protected void initChannel(SocketChannel ch) throws Exception {
+                            // add HTTP server codec and continue handler to the pipeline
                             ch.pipeline().addLast(new HttpServerCodec());
                             ch.pipeline().addLast(new HttpServerExpectContinueHandler());
+                            // add custom server handler to the pipeline
                             ch.pipeline().addLast("handler",new HuntedPazzlServerHandler());
-                            
                         }
-                        
                     });
-                    
 
+            // bind server to the specified port and start listening for incoming connections
             ChannelFuture channelFuture = serverBootstrap.bind(port).sync();
+            // wait until server channel is closed
             channelFuture.channel().closeFuture().sync();
         } finally {
+            // gracefully shutdown both event loop groups
             bossGroup.shutdownGracefully();
             workerGroup.shutdownGracefully();
         }
     }
 
     public static void main(String[] args) throws Exception {
-        
+        // check if command line arguments are provided
         if (args.length > 0) { // condition for args if mistakes occurs
+            // print usage message and exit
             System.err.println("Usage: java " + HuntedPazzlServer.class.getSimpleName() + " <port>");
             return;
         }
-        
+
+        // set default port number
         int port = Integer.parseInt("8080");
+        // create new instance of the server and start it
         new HuntedPazzlServer(port).run();
     }
 }
--- src/main/java/com/example/server/game/Haunted_puzzle.java
+++ src/main/java/com/example/server/game/Haunted_puzzle.java
@@ -24,26 +24,33 @@ public class Haunted_puzzle  implements ActionListener{
      * @param playerNameString 
      * @throws IOException
      */
+    // Creates a new game window with the player's name
     private static void Game(String playerNameString) throws IOException{
-        frame = createFrame("Haunted Puzzle Game: " + playerNameString); // Name of puzzle and player.
+        // Create the game window
+        frame = createFrame("Haunted Puzzle Game: " + playerNameString);
+        // Load the puzzle images
         myUnsolved = ImageIO.read(new File("C:/Users/me/OneDrive - University of Prince Mugrin/Construction/Project_Phase_One/demo/src/main/resources/com/example/server/game/Unsolved.jpg"));
         mySolved = ImageIO.read(new File("C:/Users/me/OneDrive - University of Prince Mugrin/Construction/Project_Phase_One/demo/src/main/resources/com/example/server/game/Solved.jpg"));
-        picLabel = new JLabel(new ImageIcon(myUnsolved)); // Set unsolved state.
+        // Set the initial puzzle state to unsolved
+        picLabel = new JLabel(new ImageIcon(myUnsolved));
 
-        
+        // Create the button panel, layered pane, and game panel
         btnPanel = createBtnPanel();
         gameLayeredPane = createGameLayeredPane();
         panel = createGamePanel();
 
+        // Create the "Solve" button and add it to the button panel
         btn = createButton("Solve");
         btn.setLocation(208, 10);
         btnPanel.add(btn);
+        // Add an action listener to the "Solve" button that shows the solved puzzle image
         btn.addActionListener(new Haunted_puzzle(){
             public void actionPerformed(ActionEvent arg0) {
+                // Change the puzzle image to the solved state
                 picLabel = new JLabel(new ImageIcon(mySolved));
                 panel = createGamePanel();
 
-
+                // Update the game panel with the solved image
                 gameLayeredPane.removeAll();
                 panel.add(picLabel);
                 gameLayeredPane.add(panel);
@@ -51,102 +58,147 @@ public class Haunted_puzzle  implements ActionListener{
                 gameLayeredPane.revalidate();
             }
         });
+
+        // Create the "Blank" button and add it to the button panel
         btn2 = createButton("Blank");
         btn2.setLocation(303, 10);
         btnPanel.add(btn2);
+        // Add an action listener to the "Blank" button that shows the unsolved puzzle image
         btn2.addActionListener(new Haunted_puzzle(){
             public void actionPerformed(ActionEvent arg0) {
-                
+                // Change the puzzle image to the unsolved state
                 picLabel = new JLabel(new ImageIcon(myUnsolved));
                 panel = createGamePanel();
 
+                // Update the game panel with the unsolved image
                 gameLayeredPane.removeAll();
                 panel.add(picLabel);
                 gameLayeredPane.add(panel);
                 gameLayeredPane.repaint();
-                gameLayeredPane.revalidate();;
-
+                gameLayeredPane.revalidate();
             }
         });
-        frame.add(btnPanel);        
-        
-        panel.add(picLabel);
-        gameLayeredPane.add(panel);
-        frame.add(gameLayeredPane);
-        frame.setVisible(true); // Visible.
-    }
+            frame.add(btnPanel);        
+            
+            panel.add(picLabel);
+            gameLayeredPane.add(panel);
+            frame.add(gameLayeredPane);
+            frame.setVisible(true); // Visible.
+        }
     /**
      * @return frame
      * Sepreate mothed: that create frame along with settings.
      */
     private static JFrame createFrame(String gameNameString) { 
+        // Set the look and feel of the JFrame to the system's default
         JFrame.setDefaultLookAndFeelDecorated(true);
+    
+        // Create a new JFrame with the given name
         frame = new JFrame(gameNameString);
+    
+        // Set the default close operation of the JFrame to exit the application
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+        // Set the size of the JFrame to 626x790 pixels
         frame.setBounds(0, 0, 626, 790);
+    
+        // Set the location of the JFrame to the center of the screen
         frame.setLocationRelativeTo(null);
+    
+        // Set the JFrame to not be resizable
         frame.setResizable(false);
+    
+        // Set the layout of the JFrame to null
         frame.setLayout(null);
+    
+        // Return the created JFrame
         return frame;
     }
+    
     /**
      * @param texString
      * @return
-     * Sepreate mothed: That create Buttons along with settings. 
+     * Separate method: Create a JButton with the given text and default settings.
      */
     private static JButton createButton(String texString) {    
+        // Create a new JButton with the given text and null icon
         button = new JButton(texString, null);
+    
+        // Set the size of the JButton to 90x20 pixels
         button.setSize(90, 20);
-
+    
+        // Return the created JButton
         return button;
     }
+    
     /**
      * @return
-     * Sepreate mothed: acts as panel Container
+     * Separate method: Create a JLayeredPane that acts as a container for the game.
      */
     private static JLayeredPane createGameLayeredPane() {
+        // Create a new JLayeredPane
         layeredPane = new JLayeredPane();
+    
+        // Set the bounds of the JLayeredPane to 0,40 and 616x750 pixels
         layeredPane.setBounds(0, 40, 616, 750);
+    
+        // Set the layout of the JLayeredPane to null
         layeredPane.setLayout(null);
+    
+        // Return the created JLayeredPane
         return layeredPane;
     }
+    
     /**
      * @return
-     * Sepreate mothed: That create a panel which holds game screan.
+     * Separate method: Create a JPanel that holds the game screen.
      */
     private static JPanel createGamePanel() {
+        // Create a new JPanel
         panel = new JPanel();
+    
+        // Set the size of the JPanel to 616x720 pixels
         panel.setSize(616, 720);
+    
+        // Set the background color of the JPanel to black
         panel.setBackground(Color.BLACK);
+    
+        // Return the created JPanel
         return panel; 
     }
+    
     /**
      * @return
-     * Sepreate mothed: That create a panel which holds action buttons.
+     * Separate method: Create a JPanel that holds the action buttons.
      */
     private static JPanel createBtnPanel() {
+        // Create a new JPanel
         panel = new JPanel();
+    
+        // Set the size of the JPanel to 616x40 pixels
         panel.setSize(616, 40);
+    
+        // Return the created JPanel
         return panel;
     }
-
+    
     public static void main( String arge[]) throws IOException{
-    Game("Player"); // testing the game
-
+        // Start the game with the default player name "Player"
+        Game("Player");
     }
+    
     /**
      * @param player
      * @throws IOException
-     * Getter mothed: Start game along with player's name.
+     * Separate method: Start the game with the given player name.
      */
     public static void stratGame(String player) throws IOException {
+        // Start the game with the given player name
         Game(player);
     }
-
-
+    
     @Override
     public void actionPerformed(ActionEvent arg0) {
-
+        // This method is not currently used, but is required to implement the ActionListener interface
     }
-    
 }
